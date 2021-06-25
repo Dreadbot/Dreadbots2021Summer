d29abb64d2eb01c1a32376a3b1922674e7e60f5b
cjohnson <collin.g.johnson@gmail.com>
2021-06-25 14:39:45 -0400

    Add new velocity-based drive functionality

--- src/main/java/frc/robot/subsystem/Drive.java
+++ src/main/java/frc/robot/subsystem/Drive.java
@@ -2,31 +2,46 @@ package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.SlewRateLimiter;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drive {
-    private CANSparkMax leftFrontMotor;
-    private CANSparkMax leftBackMotor;
-    private CANSparkMax rightFrontMotor;
-    private CANSparkMax rightBackMotor;
+    private static final MotorType K_MOTORTYPE = CANSparkMaxLowLevel.MotorType.kBrushless;
 
-    private final DifferentialDrive robotDrive;
+    // Drive Constants
+    private static final double K_MAX_SPEED = 3.0d; // meters per second
+    private static final double K_MAX_ANGULAR_SPEED = 2.0d * Math.PI; // radians per second
+
+    public static final double kTrackwidthMeters = 0.705d; // meters
+
+    // Motors
+    private CANSparkMax leftFrontMotor = new CANSparkMax(0, K_MOTORTYPE);
+    private CANSparkMax leftBackMotor = new CANSparkMax(1, K_MOTORTYPE);
+    private CANSparkMax rightFrontMotor = new CANSparkMax(2, K_MOTORTYPE);
+    private CANSparkMax rightBackMotor = new CANSparkMax(3, K_MOTORTYPE);
+
+    // Slew Rate Limiters (Prevent unintentional erratic movements) at (1/3)sec
+    private final SlewRateLimiter speedLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter rotLimiter = new SlewRateLimiter(3);
+
+    // Drive calculations class
+    private final DifferentialDrive robotDrive = new DifferentialDrive(this.leftFrontMotor, this.rightFrontMotor);
 
     public Drive() {
-        this.leftFrontMotor = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
-        this.leftBackMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-        this.rightFrontMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-        this.rightBackMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-
         // Back motors follow front motors 
         this.leftBackMotor.follow(this.leftFrontMotor);
         this.rightBackMotor.follow(this.rightFrontMotor);
-
-        this.robotDrive = new DifferentialDrive(this.leftFrontMotor, this.rightFrontMotor);
     }
 
     public void drive(double xSpeed, double zRotation) {
+        // Apply a slew rate limiter and use value as a percentage of max speed.
+        xSpeed = -speedLimiter.calculate(xSpeed) * K_MAX_SPEED;
+
+        // Apply a slew rate limiter and calculate the angular velocity
+        zRotation = -rotLimiter.calculate(zRotation) * K_MAX_ANGULAR_SPEED;
+
         this.robotDrive.arcadeDrive(xSpeed, zRotation);
     }
 }
