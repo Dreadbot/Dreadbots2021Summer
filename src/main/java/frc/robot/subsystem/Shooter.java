eb1d3ce0e52e385d6986a4b795b366d2eb918f4d
cjohnson <collin.g.johnson@gmail.com>
2021-06-25 15:33:51 -0400

    Clean up Shooter code

--- src/main/java/frc/robot/subsystem/Shooter.java
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -14,8 +14,8 @@ public class Shooter {
 	boolean upperLimitHit = false;
 
 	// Motor Controllers
-	private final CANSparkMax shooterMotor;
-	private final CANSparkMax aimingMotor;
+	private final CANSparkMax shooterMotor = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
+	private final CANSparkMax aimingMotor = new CANSparkMax(8, CANSparkMaxLowLevel.MotorType.kBrushless);
 
 	// PID Controllers
 	private final CANPIDController shooterMotorPID;
@@ -26,22 +26,19 @@ public class Shooter {
 	private final CANEncoder aimingMotorEncoder;
 
 	// Limit Switches
-	private final DigitalInput upperLimitSwitch;
-	private final DigitalInput lowerLimitSwitch;
+	private final DigitalInput upperLimitSwitch = new DigitalInput(1);
+	private final DigitalInput lowerLimitSwitch = new DigitalInput(2);
 
 	// Vision Light
-	private final Solenoid visionLEDRing;
+	private final Solenoid visionLEDRing = new Solenoid(7);
 
 	// Shooting Mechanism Variables
-	private final double speed;
 	private double aimPosition;
 	private double range;
 
 	public Shooter() {
-		// Instantiate Motor Controllers
-		shooterMotor = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
-		aimingMotor = new CANSparkMax(8, CANSparkMaxLowLevel.MotorType.kBrushless);
-		shooterMotor.restoreFactoryDefaults();
+        shooterMotor.restoreFactoryDefaults();
+        
 		// Get the PID Controller Objects
 		shooterMotorPID = shooterMotor.getPIDController();
 		aimingMotorPID = aimingMotor.getPIDController();
@@ -49,26 +46,14 @@ public class Shooter {
 		// Get the Encoder Objects
 		shooterMotorEncoder = shooterMotor.getEncoder();
 		aimingMotorEncoder = aimingMotor.getEncoder();
-
-		// Instantiate Limit Switches
-		upperLimitSwitch = new DigitalInput(1);
-		lowerLimitSwitch = new DigitalInput(2);
-
-		// Instantiate Vision LED Ring
-		visionLEDRing = new Solenoid(7);
-
-		// Configure PID Controllers (values are tuned)
-		// shooterMotorPID.setP(0.0025);
-		// shooterMotorPID.setI(0.00000033);
-		// shooterMotorPID.setD(0.03);
-
+        
+        // Tune PID values
 		shooterMotorPID.setP(9e-3);
 		shooterMotorPID.setI(1e-6);
 		shooterMotorPID.setD(0);
 		shooterMotorPID.setIZone(0);
 		shooterMotorPID.setFF(0.000015);
 		shooterMotorPID.setOutputRange(-1.0, 1.0);
-		speed = 0.0;
 
 		aimingMotorPID.setP(0.1);
 		aimingMotorPID.setI(0);
@@ -76,12 +61,10 @@ public class Shooter {
 		aimingMotorPID.setIZone(0);
 		aimingMotorPID.setFF(0.000015);
 		aimingMotorPID.setOutputRange(-1.0, 1.0);
-
-		// aimingMotor.set(0.25);
 	}
 
 	public void shoot(double rpm) {
-		shooterMotorPID.setReference(rpm, ControlType.kVelocity);
+        shooterMotorPID.setReference(rpm, ControlType.kVelocity);
 	}
 
 	public void aimHeight(double rotations) {
@@ -99,10 +82,10 @@ public class Shooter {
 	public void setHoodPercentOutput(double percentOutput) {
 		System.out.println(!lowerLimitSwitch.get());
 		System.out.println(!upperLimitSwitch.get());
-		if(getLowerLimitSwitch() && percentOutput > 0){
+		if(getLowerLimitSwitchValue() && percentOutput > 0){
 			percentOutput = 0.0;
 		}
-		else if(getUpperLimitSwitch() && percentOutput <0){
+		else if(getUpperLimitSwitchValue() && percentOutput <0){
 			percentOutput = 0.0;
 		}
 
@@ -154,44 +137,46 @@ public class Shooter {
 		aimingMotorPID.setReference(position, ControlType.kPosition);
 	}
 
-	public boolean getUpperLimitSwitch() {
+	public boolean getUpperLimitSwitchValue() {
 		return !upperLimitSwitch.get();
 	}
 
-	public boolean getLowerLimitSwitch() {
+	public boolean getLowerLimitSwitchValue() {
 		return !lowerLimitSwitch.get();
 	}
 
-	public boolean getUpperLimitBool() {
-		return upperLimitHit;
-	}
-
-	public boolean getLowerLimitBool() {
-		return lowerLimitHit;
-	}
-
 	public void hoodCalibration(){
-		if(getLowerLimitSwitch() && !getLowerLimitBool()){ 
-		  System.out.println("***************LOWER LIMIT TRIGGERED");
-		  setLowerLimit(getHoodPosition());
-		  setHoodPercentOutput(-0.75);
-		}
-		else if(getUpperLimitSwitch() && !getUpperLimitBool()){
-		  System.out.println("***************UPPER LIMIT TRIGGERED");
-		  setUpperLimit(getHoodPosition());
-		  setHoodPercentOutput(0.75);
-		}
-		else if (getUpperLimitBool() && getLowerLimitBool() && !getAimReadiness()){
-		  setAimReadiness(true);
-		  setHoodPosition(0.5);
-		}
-		if(getAimReadiness()){
-		//   position = frc::SmartDashboard::GetNumber("Hood Position", 0.5);
+        if (readyToAim) return;
+        
+        if (lowerLimitHit && upperLimitHit) {
+            // Go back to center, ready to aim.
+            readyToAim = true;
+            range = maxHoodPosition - minHoodPosition;
+            setHoodPosition(0.5);
 
-		  //std::cout << "Adjusting Position to: " << position << std::endl;
+            return;
+        }
+
+        // Approach the lower limit of the hood position.
+        if(getLowerLimitSwitchValue() && !lowerLimitHit){ 
+		  setLowerLimit(getHoodPosition());
+          
+          // Approach the lower limit
+          setHoodPercentOutput(-0.75);
+
+          return;
+        }
+
+        // Approach the upper limit of the hood position.
+		if(getUpperLimitSwitchValue() && !upperLimitHit) {
+		  setUpperLimit(getHoodPosition());
+          
+          // Approach the lower limit
+          setHoodPercentOutput(0.75);
 		}
 	}
-	public void setPID(double p, double i, double d) {
+    
+    public void setPID(double p, double i, double d) {
 		shooterMotorPID.setP(p);
 		shooterMotorPID.setI(i);
 		shooterMotorPID.setD(d);
