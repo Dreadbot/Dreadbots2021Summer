10665babb85a5af73425130abf2cb0b8a3b9b732
cjohnson <collin.g.johnson@gmail.com>
2021-06-25 20:03:01 -0400

    Add setup for drive motor controllers

--- src/main/java/frc/robot/subsystem/Drive.java
+++ src/main/java/frc/robot/subsystem/Drive.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystem;
 
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -12,15 +13,23 @@ public class Drive {
 
     // Drive Constants
     private static final double K_MAX_SPEED = 3.0d; // meters per second
+    private static final double K_MAX_ACCELERATION = 1.5d; // meters per second squared
     private static final double K_MAX_ANGULAR_SPEED = 2.0d * Math.PI; // radians per second
 
-    public static final double kTrackwidthMeters = 0.705d; // meters
+    // Voltage Feedforward Constants
+    private static final double K_S = 0.229d; // volts
+    private static final double K_V = 0.0437d; // volt seconds per meter
+    private static final double K_A = 0.00512d; // volt seconds squared per meter
+    private static final double K_P = 1.19e-10d; // meters per second
+    
+    // Robot Details
+    private static final double kTrackwidthMeters = 0.705d; // meters
 
     // Motors
-    private CANSparkMax leftFrontMotor = new CANSparkMax(0, K_MOTORTYPE);
-    private CANSparkMax leftBackMotor = new CANSparkMax(1, K_MOTORTYPE);
-    private CANSparkMax rightFrontMotor = new CANSparkMax(2, K_MOTORTYPE);
-    private CANSparkMax rightBackMotor = new CANSparkMax(3, K_MOTORTYPE);
+    private final CANSparkMax leftFrontMotor = new CANSparkMax(0, K_MOTORTYPE);
+    private final CANSparkMax leftBackMotor = new CANSparkMax(1, K_MOTORTYPE);
+    private final CANSparkMax rightFrontMotor = new CANSparkMax(2, K_MOTORTYPE);
+    private final CANSparkMax rightBackMotor = new CANSparkMax(3, K_MOTORTYPE);
 
     // Slew Rate Limiters (Prevent unintentional erratic movements) at (1/3)sec
     private final SlewRateLimiter speedLimiter = new SlewRateLimiter(3);
@@ -33,6 +42,22 @@ public class Drive {
         // Back motors follow front motors 
         this.leftBackMotor.follow(this.leftFrontMotor);
         this.rightBackMotor.follow(this.rightFrontMotor);
+        
+        // Set up motor controllers
+        this.leftFrontMotor.restoreFactoryDefaults();
+        this.leftBackMotor.restoreFactoryDefaults();
+        this.rightFrontMotor.restoreFactoryDefaults();
+        this.rightBackMotor.restoreFactoryDefaults();
+        this.leftFrontMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        this.leftBackMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        this.rightFrontMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        this.rightBackMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+
+        // Set up the PID Controllers
+        Drive.pidControllerSetup(leftFrontMotor);
+        Drive.pidControllerSetup(leftBackMotor);
+        Drive.pidControllerSetup(rightFrontMotor);
+        Drive.pidControllerSetup(rightBackMotor);
     }
 
     public void drive(double xSpeed, double zRotation) {
@@ -43,5 +68,15 @@ public class Drive {
         zRotation = -rotLimiter.calculate(zRotation) * K_MAX_ANGULAR_SPEED;
 
         this.robotDrive.arcadeDrive(xSpeed, zRotation);
+    }
+
+    private static void pidControllerSetup(CANSparkMax motor) {
+        CANPIDController pidController = motor.getPIDController();
+        pidController.setP(0.2);
+        pidController.setI(1e-4);
+        pidController.setD(1);
+        pidController.setIZone(0.1);
+        pidController.setFF(0);
+        pidController.setOutputRange(-1, 1);
     }
 }
