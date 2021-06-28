4b024353c11de5a567aabf3afc196b7d2aeaaa5a
Pizzarushed <pizzarush04@gmail.com>
2021-06-28 16:11:50 -0400

    Started adding robot Init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import frc.robot.gamestate.Autonomous;
+//import frc.robot.gamestate.Teleoperated;
+import frc.robot.subsystem.*;
+//import frc.robot.utility.Constants;
+import frc.robot.utility.DreadbotController;
+//import frc.robot.utility.logger.RobotLogger;
+
+import java.util.ArrayList;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,9 +22,60 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+  // SUBSYSTEMS
+	public Drive drive;
+	public Shooter shooter;
+	//public Intake intake;
+	//public Feeder feeder;
+	//public Manipulator manipulator;
+	//public Ultra sonic2;
+
+	// JOYSTICKS
+	public DreadbotController primaryJoystick;
+	public DreadbotController secondaryJoystick;
+
+	// TESTING ONLY
+	//public ArrayList<Subsystem> testingSubsystems;
+	public int currentTestingIndex;
+	public boolean isTestingCompleted;
+
+	// GAME STATE
+	//private Autonomous autonomous;
+  //private Teleoperated teleoperated;
   
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+
+		// Joystick Initialization
+		primaryJoystick = new DreadbotController(0);
+		secondaryJoystick = new DreadbotController(1);
+
+		// Subsystem Initialization
+		drive = new Drive();
+		//drive.tankDrive(0.1, 0.0);
+		shooter = new Shooter();
+		/*intake = new Intake();
+		feeder = new Feeder();
+		manipulator = new Manipulator(intake,
+			feeder,
+			shooter);*/
+
+		//sonic1 = new Ultra(Constants.ULTRA_PING_CHANNEL_ID, Constants.ULTRA_ECHO_CHANNEL_ID);
+		// sonic2 = new Ultra(6, 7);
+
+		// Game State Initialization
+		/*teleoperated = new Teleoperated(primaryJoystick,
+			secondaryJoystick,
+			manipulator,
+			sparkDrive);
+		autonomous = new Autonomous(sparkDrive, teleoperated.getTeleopFunctions(), manipulator, teleoperated);*/
+
+		// Testing Initialization
+		//testingSubsystems = new ArrayList<>();
+		//testingSubsystems.add(sparkDrive);
+		//testingSubsystems.add(manipulator);
+
+  }
 
   @Override
   public void robotPeriodic() {}
