fb73f78cbb363f6ce21157672354604c83c3ad77
cjohnson <collin.g.johnson@gmail.com>
2021-07-01 22:30:38 -0400

    Add AutonDrive backwards driving support

--- src/main/java/frc/robot/gamestate/routine/AutonDrive.java
+++ src/main/java/frc/robot/gamestate/routine/AutonDrive.java
@@ -2,16 +2,31 @@ package frc.robot.gamestate.routine;
 
 import frc.robot.subsystem.SparkDrive;
 
+/**
+ * Auton segment for driving forward for a given distance in meters.
+ */
 public class AutonDrive extends AutonSegment {
 	private static final double kP = 1;
 
 	private final double distanceMeters;
 	private double initialPositionMeters;
 
+	private boolean forward = true;
+
 	private final SparkDrive sparkDrive;
 
+	/**
+	 * @param distanceMeters The given distance to drive, in meters.
+	 * @param sparkDrive The drive object reference
+	 */
 	public AutonDrive(double distanceMeters, SparkDrive sparkDrive) {
 		this.distanceMeters = distanceMeters;
+		this.sparkDrive = sparkDrive;
+	}
+
+	public AutonDrive(double distanceMeters, boolean forward, SparkDrive sparkDrive) {
+		this.distanceMeters = distanceMeters;
+		this.forward = forward;
 		this.sparkDrive = sparkDrive;
 	}
 
@@ -22,12 +37,15 @@ public class AutonDrive extends AutonSegment {
 
 	@Override
 	public void autonomousPeriodic() {
-		if(Math.abs(sparkDrive.getAverageEncoderDistance()) - initialPositionMeters < distanceMeters) {
-			sparkDrive.tankDrive(0.5, 0.5, SparkDrive.DriveMode.NORMAL);
-		}  else {
-			sparkDrive.tankDrive(0.0, 0.0);
-			complete = true;
+		double distanceFromInitialPosition = Math.abs(sparkDrive.getAverageEncoderDistance() - initialPositionMeters);
+
+		if(distanceFromInitialPosition < distanceMeters) {
+			sparkDrive.tankDrive((forward ? 1.0 : -1.0) * 0.5, 0.5);
+			return;
 		}
+
+		sparkDrive.tankDrive(0.0, 0.0);
+		complete = true;
 	}
 
 	@Override
