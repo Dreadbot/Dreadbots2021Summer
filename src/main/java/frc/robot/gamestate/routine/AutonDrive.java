df8f80a5d22c31f9a0da8306371eafb64cee89ea
cjohnson <collin.g.johnson@gmail.com>
2021-07-13 08:52:08 -0400

    Quick style edits

--- src/main/java/frc/robot/gamestate/routine/AutonDrive.java
+++ src/main/java/frc/robot/gamestate/routine/AutonDrive.java
@@ -6,55 +6,55 @@ import frc.robot.subsystem.SparkDrive;
  * Auton segment for driving forward for a given distance in meters.
  */
 public class AutonDrive extends AutonSegment {
-	private static final double kP = 1;
+    private static final double kP = 1;
 
-	private final double distanceMeters;
-	private double initialPositionMeters;
+    private final double distanceMeters;
+    private double initialPositionMeters;
 
-	private boolean forward = true;
+    private boolean forward = true;
 
-	private final SparkDrive sparkDrive;
+    private final SparkDrive sparkDrive;
 
-	/**
-	 * @param distanceMeters The given distance to drive, in meters.
-	 * @param sparkDrive The drive object reference
-	 */
-	public AutonDrive(double distanceMeters, SparkDrive sparkDrive) {
-		this.distanceMeters = distanceMeters;
-		this.sparkDrive = sparkDrive;
-	}
+    /**
+     * @param distanceMeters The given distance to drive, in meters.
+     * @param sparkDrive     The drive object reference
+     */
+    public AutonDrive(double distanceMeters, SparkDrive sparkDrive) {
+        this.distanceMeters = distanceMeters;
+        this.sparkDrive = sparkDrive;
+    }
 
-	/**
-	 * @param distanceMeters The given distance to drive, in meters.
-	 * @param forward Determines if the motion is moving forward (according to motor direction) or not.
-	 * @param sparkDrive The drive object reference
-	 */
-	public AutonDrive(double distanceMeters, boolean forward, SparkDrive sparkDrive) {
-		this.distanceMeters = distanceMeters;
-		this.forward = forward;
-		this.sparkDrive = sparkDrive;
-	}
+    /**
+     * @param distanceMeters The given distance to drive, in meters.
+     * @param forward        Determines if the motion is moving forward (according to motor direction) or not.
+     * @param sparkDrive     The drive object reference
+     */
+    public AutonDrive(double distanceMeters, boolean forward, SparkDrive sparkDrive) {
+        this.distanceMeters = distanceMeters;
+        this.forward = forward;
+        this.sparkDrive = sparkDrive;
+    }
 
-	@Override
-	public void autonomousInit() {
-		initialPositionMeters = sparkDrive.getAverageEncoderDistance();
-	}
+    @Override
+    public void autonomousInit() {
+        initialPositionMeters = sparkDrive.getAverageEncoderDistance();
+    }
 
-	@Override
-	public void autonomousPeriodic() {
-		double distanceFromInitialPosition = Math.abs(sparkDrive.getAverageEncoderDistance() - initialPositionMeters);
+    @Override
+    public void autonomousPeriodic() {
+        double distanceFromInitialPosition = Math.abs(sparkDrive.getAverageEncoderDistance() - initialPositionMeters);
 
-		if(distanceFromInitialPosition < distanceMeters) {
-			sparkDrive.drive((forward ? 1.0 : -1.0) * 0.5, 0.0);
-			return;
-		}
+        if (distanceFromInitialPosition < distanceMeters) {
+            sparkDrive.drive((forward ? 1.0 : -1.0) * 0.5, 0.0);
+            return;
+        }
 
-		sparkDrive.drive(0.0, 0.0);
-		complete = true;
-	}
+        sparkDrive.drive(0.0, 0.0);
+        complete = true;
+    }
 
-	@Override
-	public void disabledInit() {
-		super.disabledInit();
-	}
+    @Override
+    public void disabledInit() {
+        super.disabledInit();
+    }
 }
--- src/main/java/frc/robot/gamestate/routine/AutonRoutine.java
+++ src/main/java/frc/robot/gamestate/routine/AutonRoutine.java
@@ -5,25 +5,21 @@ import java.util.ArrayList;
 import frc.robot.subsystem.SparkDrive;
 
 public class AutonRoutine {
-    private ArrayList<AutonSegment> segments;
-    private int segmentIndex;
-    private boolean completed;
+    private ArrayList<AutonSegment> segments = new ArrayList<>();
+    private int segmentIndex = 0;
+    private boolean completed = false;
     private SparkDrive sparkDrive;
 
     public AutonRoutine(SparkDrive sparkDrive) {
-        segments = new ArrayList<>();
-        segmentIndex = 0;
-        completed = false;
-
         this.sparkDrive = sparkDrive;
     }
-    
+
     public AutonRoutine addSegment(AutonSegment segment) {
         segments.add(segment);
 
         return this;
     }
-    
+
     public void autonomousInit() {
         sparkDrive.getGyroscope().reset();
 
@@ -31,49 +27,49 @@ public class AutonRoutine {
         segments.get(segmentIndex).autonomousInit();
     }
 
-    public void autonomousPeriodic(){
+    public void autonomousPeriodic() {
         // Prevent IndexOutOfBoundsExceptions and allows the robot to remain
-		// running after the routine is finished.
-        if(completed)
+        // running after the routine is finished.
+        if (completed)
             return;
-        
+
         // Run the current segment's autonomousPeriodic() code.
         segments.get(segmentIndex).autonomousPeriodic();
 
         // Check to see if the current segment's task has been completed
-        if(segments.get(segmentIndex).isComplete()) {
+        if (segments.get(segmentIndex).isComplete()) {
             // Move to the next segment of the routine
             segmentIndex++;
 
             // If there are no more segments in the routine, stop the execution
-			// of the autonomous logic.
-            if(segmentIndex >= segments.size()) {
+            // of the autonomous logic.
+            if (segmentIndex >= segments.size()) {
                 // Prevents the autonomous logic from being run until the next time
-				// the autonomous period starts.
+                // the autonomous period starts.
                 completed = true;
                 return;
             }
-            
+
             // If there are more segments in the routine,
-			// call the next segment's init method.
+            // call the next segment's init method.
             segments.get(segmentIndex).autonomousInit();
         }
     }
 
     /**
-	 * Called directly from Robot.disabledInit() function. Resets the routine management
-	 * data so that the routine can be run in the same on/off cycle of the robot.
-	 */
-	public void disabledInit() {
-		// Performs a reset of all the routine data so it can be run multiple times
-		// in the same on/off cycle of the robot.
-		segmentIndex = 0;
-		completed = false;
-		for (AutonSegment segment : segments) {
-			segment.setComplete(false);
-			segment.disabledInit();
-		}
-	}
+     * Called directly from Robot.disabledInit() function. Resets the routine management
+     * data so that the routine can be run in the same on/off cycle of the robot.
+     */
+    public void disabledInit() {
+        // Performs a reset of all the routine data so it can be run multiple times
+        // in the same on/off cycle of the robot.
+        segmentIndex = 0;
+        completed = false;
+        for (AutonSegment segment : segments) {
+            segment.setComplete(false);
+            segment.disabledInit();
+        }
+    }
 
     public boolean isCompleted() {
         return completed;
